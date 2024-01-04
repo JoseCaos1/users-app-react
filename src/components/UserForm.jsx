6f5f5ed2dbe14fbad8276a75d5fbb96efe422fd4
Jose <caoss2022@gmail.com>
2024-01-04 04:14:20 -0500

    maquetamos el formulario

--- src/components/UserForm.jsx
+++ src/components/UserForm.jsx
@@ -1,6 +1,27 @@
 import React, { useState } from 'react';
 
+const initialUserForm = {
+  username: '',
+  password:"",
+  email:"",
+}
+
 export const UserForm = () => {
+
+  const [ userForm, setUserForm ] = useState(initialUserForm);
+
+  const { username, password, email }= userForm;
+
+  const onInputChange=({target})=>{
+    //console.log(target.value);
+    const { name, value }=target;
+    setUserForm({
+      ...userForm,
+      [name]: value,
+    })
+    console.log(userForm);
+  }
+
   return (
     <>
       <form>
@@ -8,20 +29,27 @@ export const UserForm = () => {
           className='form-control my-3 w-75'
           placeholder='Username'
           type="text"
-          name="username"
+          name={username}
+          onChange={onInputChange}
         />
         <input
           className='form-control my-3 w-75'
           placeholder='Password'
-          type="text"
-          name="password"
+          type="password"
+          name={password}
+        onChange={onInputChange}
         />
         <input
           className='form-control my-3 w-75'
           placeholder='Email'
           type="text"
-          name="email"
+          name={email}
+          onChange={onInputChange}
         />
+        <button
+          className='btn btn-primary btn-sm'>
+        Crear
+        </button>
       </form>
     </>
   );
