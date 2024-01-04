229562d330df61df79058d44ec9b4d7666392399
Jose <caoss2022@gmail.com>
2024-01-04 03:36:32 -0500

    Iniciamos con UserApp.jsx

---
+++ src/UsersApp.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+export const UsersApp = () => {
+  return (
+    <>
+      <div className='container my-4'>
+        <h2>User App</h2>
+      </div>
+    </>
+  );
+}
