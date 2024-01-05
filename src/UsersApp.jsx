c9014071956f0273887079664c3c912b007a5a6e
Jose <caoss2022@gmail.com>
2024-01-05 00:35:21 -0500

    implementamos hook personalizado de user

--- src/UsersApp.jsx
+++ src/UsersApp.jsx
@@ -1,61 +1,20 @@
-import React, { useReducer, useState } from 'react';
 import {UserForm} from './components/UserForm';
 import {UsersList} from './components/UsersList';
-import {userReducer} from './reducers/usersReducer';
-
-const initialUsers=[
-  {
-    id:1,
-    username: 'Jose',
-    password: '12345',
-    email: 'jose@gmail.com'
-  },
-]
-
-const initialUserForm = {
-  id: 0,
-  username: '',
-  password:"",
-  email:"",
-}
+import {useUsers} from './hooks/useUsers';
 
 {/*TODO: USER APP*/}
 export const UsersApp = () => {
 
-  const [users, dispatch] =useReducer(userReducer, initialUsers);
-  const [userSelected, setUserSelected]= useState(initialUserForm);
 
-  const handlerAddUser =(user)=>{
-    //console.log(user);
+const {
+    users,
+    userSelected,
+    initialUserForm,
 
-    let type;
-
-    if (user.id ===0 ){
-      type ='addUser';
-    }else{
-      type ='updateUser';
-    }
-
-    dispatch({
-      //type: type,
-      type,
-      payload: user,
-    })
-  }
-
-  const handlerRemoveUser =(id)=>{
-    //console.log(id);
-    dispatch({
-      type: 'removeUser',
-      payload: id,
-    })
-  }
-
-  //seleccionar a usuario
-  const handlerUserSelectedForm=(user)=>{
-    console.log(user);
-    setUserSelected({ ...user })
-  }
+    handlerAddUser,
+    handlerRemoveUser,
+    handlerUserSelectedForm,
+  }=useUsers();
 
   return (
     <>
