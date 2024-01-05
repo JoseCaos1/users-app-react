1ed5eab86a0080be77b172b281d4112ec58c5797
Jose <caoss2022@gmail.com>
2024-01-05 04:12:29 -0500

    Creamos el service Auth

--- src/UsersApp.jsx
+++ src/UsersApp.jsx
@@ -1,49 +1,12 @@
-import React, { useReducer, useState } from 'react';
-import Swal from 'sweetalert2';
+import {useAuth} from './auth/hooks/useAuth';
 import {LoginPage} from './auth/pages/LoginPage';
-import {loginReducer} from './auth/reducers/loginReducer';
 import {Navbar} from './components/layout/Navbar';
 import {UsersPage} from './pages/UsersPage';
 
-//Si existe una login ya no, caso contrario si existe un login le pasamos este por defecto
-const initialLogin = JSON.parse(sessionStorage.getItem('login')) || {
-  isAuth : false,
-  user: undefined,
-}
 
 export const UserApp = () => {
 
-  const [login, dispatch]=useReducer(loginReducer, initialLogin);
-
-  const handlerLogin=({username, password})=>{
-    console.log(username + " "+ password);
-
-    if( username==="admin" && password ==="12345" ){
-      const  user = { username : 'admin'}
-      dispatch({
-        type: 'login',
-        payload: user,
-      });
-      sessionStorage.setItem('login', JSON.stringify({
-        isAuth: true,
-        user,
-      }));
-    }else{
-      Swal.fire({
-        icon: "error",
-        title: "Oops...",
-        text: "Username o password invalidos",
-      });
-    }
-  }
-
-  const handlerLogout=()=>{
-    dispatch({
-      type: 'logout',
-    });
-    sessionStorage.removeItem('login')
-  }
-
+  const { login, handlerLogin, handlerLogout }=useAuth();
   return (
     <>
       {
---
+++ src/auth/hooks/useAuth.js
@@ -0,0 +1,50 @@
+import {useReducer} from "react";
+import Swal from "sweetalert2";
+import {loginReducer} from "../reducers/loginReducer";
+import {loginUser} from "../services/authServices";
+
+//Si existe una login ya no, caso contrario si existe un login le pasamos este por defecto
+const initialLogin = JSON.parse(sessionStorage.getItem('login')) || {
+  isAuth : false,
+  user: undefined,
+}
+
+export const useAuth =()=>{
+
+  const [login, dispatch]=useReducer(loginReducer, initialLogin);
+
+  const handlerLogin=({username, password})=>{
+    const isLogin= loginUser({username, password});
+
+    if (isLogin){
+      const  user = { username : 'admin'}
+      dispatch({
+        type: 'login',
+        payload: user,
+      });
+      sessionStorage.setItem('login', JSON.stringify({
+        isAuth: true,
+        user,
+      }));
+    }else{
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Username o password invalidos",
+      });
+    }
+  }
+
+  const handlerLogout=()=>{
+    dispatch({
+      type: 'logout',
+    });
+    sessionStorage.removeItem('login')
+  }
+
+  return{
+    login,
+    handlerLogin,
+    handlerLogout,
+  }
+}
